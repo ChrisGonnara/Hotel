df69275668bfd0e87e36fa2bb8fd6157faff0a28
Christiano Gonçalves <chrispeixe00@gmail.com>
2025-12-08 17:54:48 -0300

    Trazendo todos os códigos feito pelo grupo

---
+++ quartos.h
@@ -0,0 +1,14 @@
+#ifndef QUARTOS_H
+#define QUARTOS_H
+typedef struct {
+    int numero;
+    int capacidade;
+    float valor;
+    char status;
+} Quarto;
+
+void cadastrarQuarto();
+int buscarQuarto(int numero, Quarto *q);
+void atualizarStatus(int numero, char st);
+
+#endif
---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+int dataParaDias(Data d);
+int calculaDiarias(Data e, Data s);
+
+#endif
